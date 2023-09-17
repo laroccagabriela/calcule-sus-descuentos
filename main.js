e155deb2c15b9f6e525878ca7ed37be485322850
Gabriela Larocca <laroccagabriela@gmail.com>
2023-09-17 16:55:32 -0300

    add validation for values in the inputs

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ btn.addEventListener('click', calculatePriceWidthDiscount);
 function calculatePriceWidthDiscount() {
     // (P * (100 - D)) / 100
 
-    const price = inputPrice.value;
-    const discount = inputDiscount.value;
+    const price = Number(inputPrice.value);
+    const discount = Number(inputDiscount.value);
 
+    if (!price || !discount) {
+        pResult.innerText = 'Por favor, llena el formulario';
+        return;
+    }
     const newPrice = (price * (100 - discount)) / 100;
 
     pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
